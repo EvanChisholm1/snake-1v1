beb0f709ef88e5367c6ad52ed573ccd2ef5057de
Evan Chisholm <elchisholm248@gmail.com>
2020-10-25 16:19:44 -0400

    created a working trail system next need to add randomized apple location

--- client/components/ClientSnake.js
+++ client/components/ClientSnake.js
@@ -2,6 +2,8 @@ import { useEffect, useRef, useState } from 'react';
 
 // the current players game
 export const ClientSnake = () => {
+  const [score, setScore] = useState(10);
+  const trail = useRef([]);
   const canvas = useRef(null);
   const headCoords = useRef({ x: 0, y: 0 });
   const dir = useRef({ x: 0, y: 0 });
@@ -12,12 +14,14 @@ export const ClientSnake = () => {
     const gridSize = 20;
     const tileSize = width / gridSize;
 
+    ctx.fillStyle = '#000000';
     ctx.fillRect(0, 0, width, height);
     const gameInterval = setInterval(() => {
       ctx.fillStyle = '#000000';
       ctx.fillRect(0, 0, width, height);
 
       ctx.fillStyle = '#00ff00';
+      ctx.fillRect(10 * tileSize, 10 * tileSize, tileSize, tileSize);
       ctx.fillRect(
         headCoords.current.x * tileSize,
         headCoords.current.y * tileSize,
@@ -25,14 +29,32 @@ export const ClientSnake = () => {
         tileSize
       );
 
+      while (trail.current.length > score) trail.current.pop();
+      // console.log(trail.current);
+
+      for (let i = 0; i < trail.current.length; i++) {
+        // console.log('drawing a cell');
+        ctx.fillStyle = '#00ff00';
+        const cell = trail.current[i];
+        // console.log(cell);
+        ctx.fillRect(cell.x * tileSize, cell.y * tileSize, tileSize, tileSize);
+      }
+
+      trail.current.unshift(headCoords.current);
       const { x, y } = headCoords.current;
 
-      headCoords.current = { x: x + dir.current.x, y: y + dir.current.y };
-      console.log('hello world');
+      headCoords.current = {
+        x: x + dir.current.x,
+        y: y + dir.current.y,
+      };
+
+      if (x === 10 && y === 10) {
+        setScore(score + 1);
+      }
     }, 1000 / 15);
 
     const keyHandler = e => {
-      console.log(e.key);
+      // console.log(e.key);
       switch (e.key) {
         case 'ArrowRight':
           dir.current = { x: 1, y: 0 };
@@ -56,7 +78,7 @@ export const ClientSnake = () => {
       clearInterval(gameInterval);
       window.removeEventListener('keydown', keyHandler);
     };
-  }, [dir, headCoords]);
+  }, [score]);
 
   return <canvas ref={canvas} width={500} height={500} />;
 };
